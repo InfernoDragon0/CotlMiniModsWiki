ad2c8a04b80026adb6af94e5d89578455eba1af5
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-11-28 14:25:24 +0800

    fix missing gifS?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxt/image-edge','@nuxtjs/tailwindcss'],
-    plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
+    //plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
 })
